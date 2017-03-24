2cf4fc6d699ed74850de64756b3142a358d499e4
Stuart Sierra <mail@stuartsierra.com>
2017-03-24 09:57:19 -0400

    Dvorak-style numbers & punctuation, arrows on right
    Also added "." on "E" in layer 1, close to its usual spot, as a
    convenience for entering decimal numbers.
    
    Also keeping "-" and "/" active in layer 1 for entering numbers with
    dashes or slashes.

--- keyboard/atreus/keymap_stuart.c
+++ keyboard/atreus/keymap_stuart.c
@@ -6,32 +6,20 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* 0: mostly letters */
-  KEYMAP(KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y, KC_F, KC_G, KC_C, KC_R, KC_L, \
-         KC_A, KC_O, KC_E, KC_U, KC_I, KC_D, KC_H, KC_T, KC_N, KC_S, \
-         KC_SCLN, KC_Q, KC_J, KC_K, KC_X, KC_B, KC_M, KC_W, KC_V, KC_Z, \
-         KC_ESC, KC_TAB, KC_LGUI, KC_LSFT, KC_BSPC, KC_LALT, KC_LCTL, \
-         KC_SPC, KC_FN0, KC_MINS, KC_SLSH, KC_ENT), \
-  /* 1: punctuation and numbers: like FN_ARROW_LAYER in
-     keymap_common.h but with Ctrl and Alt swapped. */
-  KEYMAP(SHIFT(KC_1), SHIFT(KC_2), KC_UP, SHIFT(KC_LBRC), SHIFT(KC_RBRC), \
-         KC_PGUP, KC_7, KC_8, KC_9, SHIFT(KC_8), \
-         SHIFT(KC_3), KC_LEFT, KC_DOWN, KC_RIGHT, SHIFT(KC_4), \
-         KC_PGDN, KC_4, KC_5, KC_6, SHIFT(KC_EQUAL), \
-         KC_LBRC, KC_RBRC, SHIFT(KC_9), SHIFT(KC_0), SHIFT(KC_7), \
-         KC_GRAVE, KC_1, KC_2, KC_3, KC_BSLS, \
-         KC_FN1, SHIFT(KC_INS), KC_LGUI, KC_LSFT, KC_BSPC, KC_LALT, \
-         KC_LCTL, KC_SPC, KC_FN0, KC_DOT, KC_0, KC_EQUAL),
-  
-  /* 2: arrows and function keys: like LAYER_TWO in keymap_common.h
-     but with Ctrl and Alt swapped. */
-  KEYMAP(KC_INS, KC_HOME, KC_UP, KC_END, KC_PGUP, \
-         KC_UP, KC_F7, KC_F8, KC_F9, KC_F10, \
-         KC_DEL, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN, \
-         KC_DOWN, KC_F4, KC_F5, KC_F6, KC_F11, \
-         KC_NO, KC__VOLUP, KC_NO, KC_NO, KC_FN3, \
-         KC_NO, KC_F1, KC_F2, KC_F3, KC_F12, \
-         KC_NO, KC__VOLDOWN, KC_LGUI, KC_LSFT, KC_BSPC, KC_LALT, \
-         KC_LCTL, KC_SPC, KC_FN2, KC_PSCREEN, KC_SLCK, KC_PAUSE)
+  KEYMAP(KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y,  KC_F, KC_G, KC_C, KC_R, KC_L, \
+         KC_A, KC_O, KC_E, KC_U, KC_I,          KC_D, KC_H, KC_T, KC_N, KC_S, \
+         KC_SCLN, KC_Q, KC_J, KC_K, KC_X,       KC_B, KC_M, KC_W, KC_V, KC_Z, \
+         KC_ESC, KC_TAB, KC_LGUI, KC_LSFT, KC_BSPC, KC_LALT, KC_LCTL, KC_SPC, KC_FN0, KC_MINS, KC_SLSH, KC_ENT),
+  /* 1: punctuation and numbers */
+  KEYMAP(KC_1, KC_2, KC_3, KC_4, KC_5,            KC_6, KC_7, KC_8, KC_9, KC_0, \
+         KC_EQUAL, KC_NO, KC_DOT, KC_NO, KC_HOME, KC_PGUP, KC_NO, KC_UP, KC_LBRACKET, KC_RBRACKET, \
+         KC_GRAVE, KC_NO, KC_NO, KC_NO, KC_END,   KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT, KC_BSLASH, \
+         KC_FN1, KC_TAB, KC_LGUI, KC_LSFT, KC_DEL, KC_LALT, KC_LCTL, KC_SPC, KC_FN0, KC_MINS, KC_SLSH, KC_ENTER),
+  /* 2: function keys */
+  KEYMAP(KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,   KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, \
+         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_F11, \
+         KC_NO, KC_NO, KC_NO, KC_NO, KC_FN3,  KC_NO, KC_NO, KC_NO, KC_NO, KC_F12, \
+         KC_FN2, KC_TAB, KC_LGUI, KC_LSFT, KC_BSPC, KC_LALT, KC_LCTL, KC_SPC, KC_FN2, KC_NO, KC_NO, KC_ENTER)
 };
 
 const uint16_t PROGMEM fn_actions[] = {
